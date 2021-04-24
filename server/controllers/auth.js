ece3ca89f6e8501ca5ed025af79c006e5c5a13a1
Ahmet Sait Çalışır <56252214+AhmetSaitCalisir@users.noreply.github.com>
2021-04-24 16:47:53 +0300

    v0.0.2
    -SERVER-
    
    Login (Without token)

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -12,6 +12,24 @@ exports.register = async (req, res, next) => {
   }
 };
 
-exports.login = (req, res, next) => {
-  res.send("Login Route");
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    res
+      .status(400)
+      .json({ success: false, error: "Please provide email and password" });
+  try {
+    const user = await User.findOne({ email }).select("+password");
+    if (!user)
+      res.status(404).json({ success: false, error: "Invalid credentials" });
+    const isMatch = await user.matchPasswords(password);
+    if (!isMatch)
+      res.status(404).json({ success: false, error: "Invalid credentials" });
+
+    res.status(200).json({ success: true, token: "tr454535" });
+  } catch (error) {
+    console.error(["Error at login", error]);
+    res.status(500).json({ success: false, error: error.message });
+  }
 };
